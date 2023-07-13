3262efa35f76eadf59ed70d19d8826a6c49b7052
shakori999 <shakori999@gmail.com>
2023-07-13 12:49:16 +0300

    update docker image and add jwt as sudo code

--- part-9/ecommerce/urls.py
+++ part-9/ecommerce/urls.py
@@ -7,6 +7,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 from ecommerce.drf.views import (
     CategoryList,
@@ -26,6 +32,8 @@ from ecommerce.order.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/inventory/categories/all/", CategoryList.as_view()),
     path("api/inventory/category/<str:pk>/", CategoryDetails.as_view()),
     path("api/inventory/products/category/<str:query>/",ProductByCategory.as_view()),
